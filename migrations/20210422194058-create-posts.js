2cc9b54819480a20673b7174a391e5f4650b5c2e
Tincho3604 <tincho3604@gmail.com>
2021-04-22 18:25:05 -0300

    Feat: Creat Models

---
+++ migrations/20210422194058-create-posts.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Título: {
+        type: Sequelize.STRING
+      },
+      Contenido: {
+        type: Sequelize.STRING
+      },
+      Imagen: {
+        type: Sequelize.STRING
+      },
+      Categoría: {
+        type: Sequelize.STRING
+      },
+      Fecha: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Posts');
+  }
+};
--- models/Posts.js
+++ models/Posts.js
@@ -1,11 +1,27 @@
-module.exports = (sequelize, Datatypes) => {
-    const User = sequelize.define("Posts", {
-        Título : {
-            type: Datatypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true
-            }
-        }
-    })
-}
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Posts extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Posts.init({
+    Título: DataTypes.STRING,
+    Contenido: DataTypes.STRING,
+    Imagen: DataTypes.STRING,
+    Categoría: DataTypes.STRING,
+    Fecha: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Posts',
+  });
+  return Posts;
+};
